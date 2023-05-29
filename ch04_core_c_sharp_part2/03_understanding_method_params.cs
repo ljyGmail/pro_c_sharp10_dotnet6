bb173531561e72c5f6dfab3583bf0fca2d9b16b6
liangjinyong <liangjy881@gmail.com>
2023-05-29 14:23:44 +0900

    method parameters finished

--- ch04_core_c_sharp_part2/03_understanding_method_params.cs
+++ ch04_core_c_sharp_part2/03_understanding_method_params.cs
@@ -76,4 +76,97 @@ class FunWithMethodParameters
         int ans = x + y;
         return ans;
     }
+
+    public static double CalculateAverage(params double[] values)
+    {
+        Console.WriteLine("You sent me {0} double arguments.", values.Length);
+
+        double sum = 0;
+        if (values.Length == 0)
+        {
+            return sum;
+        }
+        for (int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+        return (sum / values.Length);
+    }
+
+    public static void EnterLogData(string message, string owner = "Programmer")
+    {
+        Console.WriteLine("Error: {0}", message);
+        Console.WriteLine("Owner of Error: {0}", owner);
+    }
+
+    /*
+    // Error! The default value for an optional argument must be know at compile time!
+    public static void EnterLogData(string message, string owner = "Programmer", DateTime timeStamp = DateTime.Now)
+    {
+        Console.WriteLine("Error: {0}", message);
+        Console.WriteLine("Owner of Error: {0}", owner);
+        Console.WriteLine("Time of Error: {0}", timeStamp);
+    }
+    */
+
+    // Note: To avoid ambiguity, optional parameters must always be placed at the end of a method signature.
+
+    public static void DisplayFancyMessage(ConsoleColor textColor, ConsoleColor backgroundColor, string message)
+    {
+        // Store old colors to restore after message is printed.
+        ConsoleColor oldTextColor = Console.ForegroundColor;
+        ConsoleColor oldBackgroundColor = Console.BackgroundColor;
+        // Set new colors and print message.
+        Console.ForegroundColor = textColor;
+        Console.BackgroundColor = backgroundColor;
+        Console.WriteLine(message);
+        // Restore previous colors.
+        Console.ForegroundColor = oldTextColor;
+        Console.BackgroundColor = oldBackgroundColor;
+    }
+
+    // Method overloading
+    public static int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+
+    public static int Multiply(int x, int y, int z = 5)
+    {
+        return x * y * z;
+    }
+
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public static long Multiply(long x, long y)
+    {
+        return x * y;
+    }
+
+    /*
+    // These can not exist at the same time
+    public static int Multiply(ref int x) { return 0; }
+    public static int Multiply(out int x) { x = 0; return 0; }
+    */
+
+    // OK, if only one method uses in, ref, or out, the compiler can distinguish between the signatures.
+    public static int Multiply(ref int x) { return 0; };
+    public static int Multiply(int x) { return 0; };
+
+    // Checking Parameters for Null (Updated 10.0)
+    public static void EnterLogData2(string message, string owner = "Programmer")
+    {
+        /*
+        if (message == null)
+        {
+            throw new ArgumentNullException(message);
+        }
+        */
+        ArgumentNullException.ThrowIfNull(message);
+        Console.WriteLine("Error: {0}", message);
+        Console.WriteLine("Owner of Error: {0}", owner);
+    }
 }
--- ch04_core_c_sharp_part2/Program.cs
+++ ch04_core_c_sharp_part2/Program.cs
@@ -41,3 +41,57 @@
 // Console.WriteLine("Before: {0}, {1}", str1, str2);
 // FunWithMethodParameters.SwapStrings(ref str1, ref str2);
 // Console.WriteLine("After: {0}, {1}", str1, str2);
+
+// Pass in a comma-delimited list of doubles...
+// double average;
+// average = FunWithMethodParameters.CalculateAverage(4.0, 3.2, 5.7, 64.22, 87.2);
+// Console.WriteLine("Average of data is: {0}", average);
+// .. or pass an array of doubles.
+// double[] data = { 4.0, 3.2, 5.7 };
+// average = FunWithMethodParameters.CalculateAverage(data);
+// Console.WriteLine("Average of data is {0}", average);
+
+// Average of 0 is 0!
+// Console.WriteLine("Average of data is {0}", FunWithMethodParameters.CalculateAverage());
+
+// Defining Optional Parameters
+// FunWithMethodParameters.EnterLogData("Oh no! Grid can't find data");
+// FunWithMethodParameters.EnterLogData("Oh no! I can't find the payroll data", "CFO");
+
+// Using Named Arguments (Updated 7.2)
+/*
+FunWithMethodParameters.DisplayFancyMessage(message: "Wow! Very Fancy indeed!",
+    textColor: ConsoleColor.DarkRed,
+    backgroundColor: ConsoleColor.White);
+
+FunWithMethodParameters.DisplayFancyMessage(backgroundColor: ConsoleColor.Green,
+    message: "Testing...",
+    textColor: ConsoleColor.DarkBlue);
+
+// This is OK, as positional args are listed before named args.
+FunWithMethodParameters.DisplayFancyMessage(ConsoleColor.Blue,
+    message: "Testing...",
+    backgroundColor: ConsoleColor.White);
+
+// This is OK, all arguments are in the correct order.
+FunWithMethodParameters.DisplayFancyMessage(textColor: ConsoleColor.White,
+    backgroundColor: ConsoleColor.Blue,
+    "Testing...");
+
+// This is an ERROR, as positional args are listed after named args.
+// FunWithMethodParameters.DisplayFancyMessage(message: "Testing...",
+//     backgroundColor: ConsoleColor.White,
+//     ConsoleColor.Blue);
+*/
+
+// Console.WriteLine("***** Fun with Method Overloading *****\n");
+
+// Calls int version of Multiply()
+// This will invoke Multipy(int x, int y) version, not Multipy(int x, int y, intz).
+// Console.WriteLine(FunWithMethodParameters.Multiply(10, 10));
+
+// Calls long version of Multiply() (using the new digit separator)
+// Console.WriteLine(FunWithMethodParameters.Multiply(900_000_000_000, 900_000_000_000));
+
+// Calls double version of Multiply()
+// Console.WriteLine(FunWithMethodParameters.Multiply(4.3, 4.4));.
